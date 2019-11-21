5526ba0ff1a72a3111f09647e5c9aad7d13976a3
vgupta123 <keviv9@gmail.com>
2019-11-21 00:41:07 -0700

    minor

--- run.sh
+++ run.sh
@@ -3,12 +3,11 @@ python jemdoc.py -c analytics.conf index
 python jemdoc.py -c analytics.conf about
 python jemdoc.py -c analytics.conf pubs
 python jemdoc.py -c analytics.conf intern
-# python jemdoc.py -c analytics.conf nlpprojs
-# python jemdoc.py -c analytics.conf mlprojs
 python jemdoc.py -c analytics.conf talks
 python jemdoc.py -c analytics.conf awards
 python jemdoc.py -c analytics.conf service
 python jemdoc.py -c analytics.conf mentor
 python jemdoc.py -c analytics.conf thesis
 python jemdoc.py -c analytics.conf contact
-
+# python jemdoc.py -c analytics.conf nlpprojs
+# python jemdoc.py -c analytics.conf mlprojs
