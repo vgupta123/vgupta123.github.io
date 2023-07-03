960ab83fd5cab69f268c556877f68829b922f148
vgupta123 <keviv9@gmail.com>
2023-07-03 15:02:43 -0600

    minor

--- run.sh
+++ run.sh
@@ -7,7 +7,8 @@ python jemdoc.py -c analytics.conf talks
 python jemdoc.py -c analytics.conf awards
 python jemdoc.py -c analytics.conf service
 python jemdoc.py -c analytics.conf mentor
-python jemdoc.py -c analytics.conf thesis
+python jemdoc.py -c analytics.conf master
+python jemdoc.py -c analytics.conf phdthesis
 python jemdoc.py -c analytics.conf contact
 # python jemdoc.py -c analytics.conf nlpprojs
 # python jemdoc.py -c analytics.conf mlprojs
